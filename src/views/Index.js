3fad79144d425b06026162a8e0d111f3569c00b6
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-06-12 16:28:09 +0300

    making portfolio

--- src/views/Index.js
+++ src/views/Index.js
@@ -15,36 +15,36 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
+import React from 'react';
 
 // reactstrap components
-import { Container, Row } from "reactstrap";
+import { Container, Row } from 'reactstrap';
 
 // core components
-import DemoNavbar from "components/Navbars/DemoNavbar.js";
-import CardsFooter from "components/Footers/CardsFooter.js";
+import DemoNavbar from 'components/Navbars/DemoNavbar.js';
+import CardsFooter from 'components/Footers/CardsFooter.js';
 
 // index page sections
-import Hero from "./IndexSections/Hero.js";
-import Buttons from "./IndexSections/Buttons.js";
-import Inputs from "./IndexSections/Inputs.js";
-import CustomControls from "./IndexSections/CustomControls.js";
-import Menus from "./IndexSections/Menus.js";
-import Navbars from "./IndexSections/Navbars.js";
-import Tabs from "./IndexSections/Tabs.js";
-import Progress from "./IndexSections/Progress.js";
-import Pagination from "./IndexSections/Pagination.js";
-import Pills from "./IndexSections/Pills.js";
-import Labels from "./IndexSections/Labels.js";
-import Alerts from "./IndexSections/Alerts.js";
-import Typography from "./IndexSections/Typography.js";
-import Modals from "./IndexSections/Modals.js";
-import Datepicker from "./IndexSections/Datepicker.js";
-import TooltipPopover from "./IndexSections/TooltipPopover.js";
-import Carousel from "./IndexSections/Carousel.js";
-import Icons from "./IndexSections/Icons.js";
-import Login from "./IndexSections/Login.js";
-import Download from "./IndexSections/Download.js";
+import Hero from './IndexSections/Hero.js';
+import Buttons from './IndexSections/Buttons.js';
+import Inputs from './IndexSections/Inputs.js';
+import CustomControls from './IndexSections/CustomControls.js';
+import Menus from './IndexSections/Menus.js';
+import Navbars from './IndexSections/Navbars.js';
+import Tabs from './IndexSections/Tabs.js';
+import Progress from './IndexSections/Progress.js';
+import Pagination from './IndexSections/Pagination.js';
+import Pills from './IndexSections/Pills.js';
+import Labels from './IndexSections/Labels.js';
+import Alerts from './IndexSections/Alerts.js';
+import Typography from './IndexSections/Typography.js';
+import Modals from './IndexSections/Modals.js';
+import Datepicker from './IndexSections/Datepicker.js';
+import TooltipPopover from './IndexSections/TooltipPopover.js';
+import Carousel from './IndexSections/Carousel.js';
+import Icons from './IndexSections/Icons.js';
+import Login from './IndexSections/Login.js';
+// import Download from './IndexSections/Download.js';
 
 class Index extends React.Component {
   componentDidMount() {
@@ -56,25 +56,25 @@ class Index extends React.Component {
     return (
       <>
         <DemoNavbar />
-        <main ref="main">
+        <main ref='main'>
           <Hero />
           <Buttons />
           <Inputs />
-          <section className="section">
+          <section className='section'>
             <Container>
               <CustomControls />
               <Menus />
             </Container>
           </section>
           <Navbars />
-          <section className="section section-components">
+          <section className='section section-components'>
             <Container>
               <Tabs />
-              <Row className="row-grid justify-content-between align-items-center mt-lg">
+              <Row className='row-grid justify-content-between align-items-center mt-lg'>
                 <Progress />
                 <Pagination />
               </Row>
-              <Row className="row-grid justify-content-between">
+              <Row className='row-grid justify-content-between'>
                 <Pills />
                 <Labels />
               </Row>
@@ -88,7 +88,7 @@ class Index extends React.Component {
           <Carousel />
           <Icons />
           <Login />
-          <Download />
+          {/* <Download /> */}
         </main>
         <CardsFooter />
       </>
--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -15,15 +15,15 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
+import React from 'react';
 
 // reactstrap components
-import { Button, Card, Container, Row, Col } from "reactstrap";
+import { Button, Card, Container, Row, Col } from 'reactstrap';
 
 // core components
-import DemoNavbar from "components/Navbars/DemoNavbar.js";
-import SimpleFooter from "components/Footers/SimpleFooter.js";
-import profile from "assets/img/theme/team-4-800x800.jpg";
+import DemoNavbar from 'components/Navbars/DemoNavbar.js';
+import SimpleFooter from 'components/Footers/SimpleFooter.js';
+import profile from 'assets/img/Nawal/nawal.jpg';
 
 class Profile extends React.Component {
   componentDidMount() {
@@ -35,10 +35,10 @@ class Profile extends React.Component {
     return (
       <>
         <DemoNavbar />
-        <main className="profile-page" ref="main">
-          <section className="section-profile-cover section-shaped my-0">
+        <main className='profile-page' ref='main'>
+          <section className='section-profile-cover section-shaped my-0'>
             {/* Circles background */}
-            <div className="shape shape-style-1 shape-default alpha-4">
+            <div className='shape shape-style-1 shape-default alpha-4'>
               <span />
               <span />
               <span />
@@ -48,111 +48,116 @@ class Profile extends React.Component {
               <span />
             </div>
             {/* SVG separator */}
-            <div className="separator separator-bottom separator-skew">
+            <div className='separator separator-bottom separator-skew'>
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
+                xmlns='http://www.w3.org/2000/svg'
+                preserveAspectRatio='none'
+                version='1.1'
+                viewBox='0 0 2560 100'
+                x='0'
+                y='0'
               >
                 <polygon
-                  className="fill-white"
-                  points="2560 0 2560 100 0 100"
+                  className='fill-white'
+                  points='2560 0 2560 100 0 100'
                 />
               </svg>
             </div>
           </section>
-          <section className="section">
+          <section className='section'>
             <Container>
-              <Card className="card-profile shadow mt--300">
-                <div className="px-4">
-                  <Row className="justify-content-center">
-                    <Col className="order-lg-2" lg="3">
-                      <div className="card-profile-image">
-                        <a href="#pablo" onClick={e => e.preventDefault()}>
+              <Card className='card-profile shadow mt--300'>
+                <div className='px-4'>
+                  <Row className='justify-content-center'>
+                    <Col className='order-lg-2' lg='3'>
+                      <div className='card-profile-image'>
+                        <a href='#pablo' onClick={(e) => e.preventDefault()}>
                           <img
-                            alt="..."
-                            className="rounded-circle"
+                            alt='...'
+                            className='rounded-circle'
                             src={profile}
                           />
                         </a>
                       </div>
                     </Col>
                     <Col
-                      className="order-lg-3 text-lg-right align-self-lg-center"
-                      lg="4"
+                      className='order-lg-3 text-lg-right align-self-lg-center'
+                      lg='4'
                     >
-                      <div className="card-profile-actions py-4 mt-lg-0">
+                      <div className='card-profile-actions py-4 mt-lg-0'>
                         <Button
-                          className="mr-4"
-                          color="info"
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                          size="sm"
+                          className='mr-4'
+                          color='info'
+                          title='on GitHub'
+                          href='https://github.com/nawal-ahmad'
+                          // onClick={(e) => e.preventDefault()}
+                          size='sm'
                         >
                           Connect
                         </Button>
                         <Button
-                          className="float-right"
-                          color="default"
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                          size="sm"
+                          className='float-right'
+                          color='default'
+                          title='on LinkedIn'
+                          href='https://www.linkedin.com/in/nawal-abu-alshaikh-40982842/'
+                          // onClick={(e) => e.preventDefault()}
+                          size='sm'
                         >
                           Message
                         </Button>
                       </div>
                     </Col>
-                    <Col className="order-lg-1" lg="4">
-                      <div className="card-profile-stats d-flex justify-content-center">
+                    <Col className='order-lg-1' lg='4'>
+                      <div className='card-profile-stats d-flex justify-content-center'>
                         <div>
-                          <span className="heading">22</span>
-                          <span className="description">Friends</span>
+                          <span className='heading'>3</span>
+                          <span className='description'>Projects</span>
                         </div>
                         <div>
-                          <span className="heading">10</span>
-                          <span className="description">Photos</span>
+                          <span className='heading'>17</span>
+                          <span className='description'>Repositories</span>
                         </div>
                         <div>
-                          <span className="heading">89</span>
-                          <span className="description">Comments</span>
+                          <span className='heading'>5</span>
+                          <span className='description'>Experience</span>
                         </div>
                       </div>
                     </Col>
                   </Row>
-                  <div className="text-center mt-5">
+                  <div className='text-center mt-5'>
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Nawal Abu-alShaikh{' '}
+                      <span className='font-weight-light'>, 30</span>
                     </h3>
-                    <div className="h6 font-weight-300">
-                      <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                    <div className='h6 font-weight-300'>
+                      <i className='ni location_pin mr-2' />
+                      Amman, Jordan
                     </div>
-                    <div className="h6 mt-4">
-                      <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                    <div className='h6 mt-4'>
+                      <i className='ni business_briefcase-24 mr-2' />
+                      Biomedical Engineer, Web Developer Student
                     </div>
                     <div>
-                      <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      <i className='ni education_hat mr-2' />
+                      Jordan University of Science and Technology
                     </div>
                   </div>
-                  <div className="mt-5 py-5 border-top text-center">
-                    <Row className="justify-content-center">
-                      <Col lg="9">
+                  <div className='mt-5 py-5 border-top text-center'>
+                    <Row className='justify-content-center'>
+                      <Col lg='9'>
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                          Motivated and determine engineer, After about five
+                          years of experience in the medical market as a Middle
+                          East Sales Coordinator at Angelantoni Life Science, I
+                          recently started planning to make a career shift and
+                          get into the software development world, I hope to
+                          become a Full Stack Developer with professional
+                          personal and business skills. Appreciate competence,
+                          team spirit and growth
                         </p>
-                        <a href="#pablo" onClick={e => e.preventDefault()}>
+                        {/* <a href='#pablo' onClick={(e) => e.preventDefault()}>
                           Show more
-                        </a>
+                        </a> */}
                       </Col>
                     </Row>
                   </div>
